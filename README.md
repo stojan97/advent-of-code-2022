ef0bb4ae2969b05a03afbbdf840b0cb03d398268
stojan97 <stojan.samojlovski@gmail.com>
2022-11-29 23:34:06 +0100

    day 1 prep

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent Of Code 2022
+Advent Of Code 2022 Solutions in Python
---
+++ day-1/solution.py
@@ -0,0 +1,17 @@
+def get_input():
+    with open('input') as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+input = get_input()
+print('Part 1:', part1(input))
+print('Part 2:', part2(input))
