d660a76259a3bac506037938782143a905af788f
stojan97 <stojan.samojlovski@gmail.com>
2022-12-01 15:11:46 +0100

    day 1 solution

--- day-1/solution.py
+++ day-1/solution.py
@@ -1,17 +1,28 @@
 def get_input():
+    lines = []
+    new_list = []
     with open('input') as file:
-        lines = [line.strip() for line in file]
+
+        for line in file:
+            line_stripped = line.strip()
+            if line_stripped == '':
+                lines.append(new_list)
+                new_list = []
+            else:
+                new_list.append(int(line_stripped))
+
+    lines.append(new_list)
     return lines
 
 
 def part1(input):
-    pass
+    return max(sum(i) for i in input)
 
 
 def part2(input):
-    pass
+    return sum(sorted([sum(i) for i in input], reverse=True)[:3])
 
 
-input = get_input()
-print('Part 1:', part1(input))
-print('Part 2:', part2(input))
+inp = get_input()
+print('Part 1:', part1(inp))
+print('Part 2:', part2(inp))
