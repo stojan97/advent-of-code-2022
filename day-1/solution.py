41790ce60573c11a737d2b9981f8224e6995b2f9
stojan97 <stojan.samojlovski@gmail.com>
2022-12-02 18:05:43 +0100

    day 1 solution

--- day-1/solution.py
+++ day-1/solution.py
@@ -15,14 +15,16 @@ def get_input():
     return lines
 
 
-def part1(input):
+input = get_input()
+
+
+def part1():
     return max(sum(i) for i in input)
 
 
-def part2(input):
+def part2():
     return sum(sorted([sum(i) for i in input], reverse=True)[:3])
 
 
-inp = get_input()
-print('Part 1:', part1(inp))
-print('Part 2:', part2(inp))
+print('Part 1:', part1())
+print('Part 2:', part2())
