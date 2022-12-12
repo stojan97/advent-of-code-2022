c4f3db186a52a85a9bcfbc849217de7f4f814e8e
stojan97 <stojan.samojlovski@gmail.com>
2022-12-12 01:21:39 +0100

    day 11 solution

--- day-11/solution.py
+++ day-11/solution.py
@@ -1,21 +1,24 @@
 import re
+import time
 from collections import deque
 from math import prod, lcm
-from typing import Deque
+from typing import Deque, Callable
 
 
 class Monkey:
 
-    def __init__(self, items: Deque[int], op: str, div: int, true_throw: int, false_throw: int):
+    def __init__(self, items: Deque[int], op: Callable, multiplicand: int | None, div: int, true_throw: int, false_throw: int):
         self.items = items
         self.op = op
         self.div = div
         self.true_throw = true_throw
         self.false_throw = false_throw
         self.inspects = 0
+        self.multiplicand = multiplicand
 
     def operation(self, old):
-        return eval(self.op)
+        m = self.multiplicand if self.multiplicand else old
+        return self.op(old, m)
 
     def calculate_throws(self, part, mod):
         throws = []
@@ -33,7 +36,10 @@ class Monkey:
 def get_last_from_split(s):
     return int(s.split(' ')[-1])
 
-
+op_resolve = {
+    '*': lambda x, y: x * y,
+    '+': lambda x, y: x + y
+}
 def get_input():
     with open('input') as file:
         lines = [line.strip() for line in file]
@@ -42,12 +48,15 @@ def get_input():
     mod = 1
     for i in range(0, len(lines), 7):
         items = deque(map(int, re.findall(r'[+-]?\d+', lines[i + 1])))
-        op = 'old' + ''.join(lines[i + 2].split(' ')[-2:])
+        op = lines[i + 2].split(' ')[-2:]
+        op_cmd = op_resolve[op[0]]
+        multiplicand = int(op[1]) if op[1] != 'old' else None
+
         div = get_last_from_split(lines[i + 3])
         true_throw = get_last_from_split(lines[i + 4])
         false_throw = get_last_from_split(lines[i + 5])
         mod = lcm(mod, div)
-        monkeys.append(Monkey(items, op, div, true_throw, false_throw))
+        monkeys.append(Monkey(items, op_cmd, multiplicand, div, true_throw, false_throw))
 
     return mod, monkeys
 
@@ -64,5 +73,8 @@ def solve(inp, n, part):
     return prod(sorted([m.inspects for m in monkeys])[-2:])
 
 
-print('Part 1:', solve(get_input(), 20, 1))
+_ = get_input()
+print('Part 1:', solve(_, 20, 1))
+start = time.time()
 print('Part 2:', solve(get_input(), 10_000, 2))
+print(time.time() - start)
