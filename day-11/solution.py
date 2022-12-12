22bbf220b51c1f823a91a39b9999ac2cc1acad51
stojan97 <stojan.samojlovski@gmail.com>
2022-12-12 01:22:04 +0100

    day 11 solution

--- day-11/solution.py
+++ day-11/solution.py
@@ -76,5 +76,6 @@ def solve(inp, n, part):
 _ = get_input()
 print('Part 1:', solve(_, 20, 1))
 start = time.time()
+# 0.3 ms
 print('Part 2:', solve(get_input(), 10_000, 2))
 print(time.time() - start)
